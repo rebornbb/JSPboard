c3e70057b66e02e4cf63786928062aca06e2eefb
narye <narye@DESKTOP-HTRBJAR>
2023-05-18 17:13:41 +0900

    first commit

---
+++ BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,65 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	
+	
+	private Connection conn; //자바와 데이터베이스 연결
+	private PreparedStatement pstmt; //쿼리문 대기 및 설정
+	private ResultSet rs; //결과값 받아오기
+	
+	public UserDAO() {
+		 try {
+			String dbURL = "jdbc:mariadb://localhost:3306/bbs"; //mariaDB와 연결시켜주는 주소
+			String dbID = "root"; //mariaDB계정
+			String dbPassword = "skfodi"; //mariaDB비밀번호
+			Class.forName("org.mariadb.jdbc.Driver"); //JDBC연결클래스를 string타입으로 불러온다
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword); 
+			//드라이버 매니저에, 미리 저장했던 연결URL과 DB계정 정보를 담아 연결 설정을한다
+		 }catch(Exception e) {
+			e.printStackTrace();
+		 }
+	}
+	
+    public int login(String userID, String userPassword) {
+        String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+        try {
+            pstmt = conn.prepareStatement(SQL); //sql쿼리문을 대기시킨다.
+            pstmt.setString(1, userID); //첫번째 ?에 매개변수로 받아온 userID를 대입
+            rs = pstmt.executeQuery(); //쿼리 실행한결과
+            if(rs.next()){
+                if(rs.getString(1).equals(userPassword))
+                    return 1;  // 로그인 성공
+                else
+                    return 0; // 비밀번호 불일치
+            }
+            return -1; // ID가 없음
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -2; // DB 오류 
+    }
+    
+    public int join(User user) {
+    	  String sql = "insert into user values(?, ?, ?, ?, ?)";
+    	  try {
+    	    pstmt = conn.prepareStatement(sql);
+    	    pstmt.setString(1, user.getUserID());
+    	    pstmt.setString(2, user.getUserPassword());
+    	    pstmt.setString(3, user.getUserName());
+    	    pstmt.setString(4, user.getUserGender());
+    	    pstmt.setString(5, user.getUserEmail());
+    	    return pstmt.executeUpdate();
+    	  }catch (Exception e) {
+    	 	e.printStackTrace();
+    	  }
+    	  return -1;
+    	}
+    
+    
+}
---
+++ BBS/src/main/webapp/loginAction.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%@ page import="user.UserDAO" %>
+<%@ page import="java.io.PrintWriter" %>
+<% request.setCharacterEncoding("UTF-8"); %>
+<jsp:useBean id="user" class="user.User" scope="page"></jsp:useBean><!-- 빈을 생성한다. -->
+<jsp:setProperty name="user" property="userID"/><!-- 빈에 값을 저장한다. -->
+<jsp:setProperty name="user" property="userPassword" />
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>로그인액션</title>
+</head>
+<body>
+	<%
+		UserDAO userDAO=new UserDAO();//하나의 인스턴스
+		int result=userDAO.login(user.getUserID(), user.getUserPassword());//페이지에 입력된 아이디와 비번을 login함수에 넣어줌
+		if(result==1){ //로그인성공
+			PrintWriter script=response.getWriter();
+			script.println("<script>");
+			script.println("location.href='main.jsp'");//로그인에 성공하면 main페이지로
+			script.println("</script>");
+		}
+		else if(result==0){ //로그인실패
+			PrintWriter script=response.getWriter();
+			script.println("<script>");
+			script.println("alert('비밀번호가 틀립니다.')");
+			script.println("history.back()");
+			script.println("</script>");
+		}
+		else if(result==-1){ //존재하지않은아이디
+			PrintWriter script=response.getWriter();
+			script.println("<script>");
+			script.println("alert('존재하지 않는 아이디입니다.')");
+			script.println("history.back()");
+			script.println("</script>");
+		}
+		else if(result==-2){ //데이터베이스오류
+			PrintWriter script=response.getWriter();
+			script.println("<script>");
+			script.println("alert('데이터베이스 오류가 발생했습니다.')");
+			script.println("history.back()");
+			script.println("</script>");
+		}
+	%>
+</body>
+</html>
